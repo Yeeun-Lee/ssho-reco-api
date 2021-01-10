e77fac8ca4166a8743de817c6b3ef558b91bab05
yeeun-lee <yeeun3430@gmail.com>
2021-01-10 15:32:44 +0900

    전처리 단위 변경(item)

--- features/translation.py
+++ features/translation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import requests
-import json
-import time
-from datetime import datetime
+# import json
+# import time
+# from datetime import datetime
 
 import re
 from google_trans_new import google_translator
@@ -41,34 +41,48 @@ def getItems():
 
 def cleanTitle(text):
     text = re.sub(" /.+$| \\[.+\\]| [A-Z]+? \\[.+\\]|^[A-Za-z]+[0-9]+? | -.+$", "", text)
-
     # remove punctuations
     text = re.sub(r"[^\w\s]", "", text)
     return text
 
-def finalItems(file = None):
-    date = str(datetime.now().strftime("%y%m%d"))
-    if file==None:
-        items = getItems()
-        with open("processed_{}.json".format(date), "w") as f:
-            json.dump(items, f, ensure_ascii=False)
-    else:
-        with open(file, "r") as f:
-            items = json.load(f)
+def transItem(item):
+    item['translated'] = toEng(cleanTitle(item['title']))
+    return item
 
-    for i in range(len(items)):
-        # items[i]['_title'] = cleanTitle(items[i]['title'])
-        print("==== itemId : {}, count : {}".format(items[i]['id'], i+1))
-        print("Original title : ", items[i]['title'])
-        new = toEng(items[i]['title'])
-        print("Translated : ", new)
-        items[i]['translated'] = new
-        print()
-
-    with open("translated_{}.json".format(date), "w") as f:
-        json.dump(items, f, ensure_ascii=False)
-
-    return items
+# def finalItems(file = None):
+#     date = str(datetime.now().strftime("%y%m%d"))
+#     if file==None:
+#         items = getItems()
+#         with open("processed_{}.json".format(date), "w") as f:
+#             json.dump(items, f, ensure_ascii=False)
+#     else:
+#         with open(file, "r") as f:
+#             items = json.load(f)
+#
+#     for i in range(len(items)):
+#         # items[i]['_title'] = cleanTitle(items[i]['title'])
+#         print("==== itemId : {}, count : {}".format(items[i]['id'], i+1))
+#         print("Original title : ", items[i]['title'])
+#         new = toEng(items[i]['title'])
+#         print("Translated : ", new)
+#         items[i]['translated'] = new
+#         print()
+#
+#     with open("translated_{}.json".format(date), "w") as f:
+#         json.dump(items, f, ensure_ascii=False)
+#
+#     return items
 
 if __name__=="__main__":
-    finalItems()
+    sample = {"id": "0001221668",
+              "category": "탑",
+              "mallNo": "0001",
+              "mallNm": "스타일난다",
+              "title": "미니메시 오프숄더 스모크",
+              "price": "29000",
+              "tagList": [{"id": "fc894811d7454cd89ba0f2c21b91d942", "name": "캐주얼"},
+                          {"id": "990f3147a4544a05aaa5fd1abb095312", "name": "가을"}]}
+
+    out = transItem(sample)
+    print("original : ", out['title'])
+    print("translated out : ", out['translated'])
